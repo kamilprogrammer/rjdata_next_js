95387f6d70f159e4121e0b23f7f90eebc2e13d95
Kamel Rifai <kamilref00@gmail.com>
2025-02-01 02:25:23 +0300

    pls again

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import React from "react";
 import { Timeline } from "@/app/components/ui/timeline";
 {
-  /*HI PLS WORK PLS PLS PLS PLS */
+  /*HI PLS WORK PLS PLS PLS PLS I SAID PLS */
 }
 const data = [
   {
